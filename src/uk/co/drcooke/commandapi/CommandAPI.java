d9eee8969ad9b223e3d95c2041e21ef930699f76
David Cooke <cookedavid637@gmail.com>
2017-10-18 22:01:37 +0100

    Start register method, add main and noinherit annotations

--- src/uk/co/drcooke/commandapi/CommandAPI.java
+++
@@ -1,4 +0,0 @@
-package uk.co.drcooke.commandapi;
-
-public class CommandAPI {
-}
---
+++ src/uk/co/drcooke/commandapi/annotations/Main.java
@@ -0,0 +1,11 @@
+package uk.co.drcooke.commandapi.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Main {
+}
